2296562ff416f244ffaf650c093dcbbf5673b5e4
Jeff Stubler <brunsa2@gmail.com>
2013-09-04 22:03:49 -0500

    Save files in case of accidental deletion during makefile work for test

--- makefile
+++ makefile
@@ -1,44 +1,69 @@
+EXEC = os
+TESTEXEC = test-os
+
+DEVICE = atmega328p
 PROGRAMMER = avrispmkII
 PROGRAMMER_PORT = usb
 PROGRAMMED_DEVICE = m328p
 CLOCK = 16000000
-DEVICE = atmega328p
 FUSES = -U lfuse:w:0xf7:m -U hfuse:w:0xd9:m -U efuse:w:0x07:m
 
 SOURCE_DIRECTORY = src/
 BUILD_DIRECTORY = bin/
+TEST_DIRECTORY = test/
 
-SOURCES := $(wildcard $(SOURCE_DIRECTORY)*.c) $(wildcard $(SOURCE_DIRECTORY)network/*.c)
-OBJECTS := $(patsubst %.c,%.o,$(SOURCES))
+SOURCES := $(wildcard $(SOURCE_DIRECTORY)*.c)
+TEST_SOURCES = $(wildcard $(TEST_DIRECTORY)*.c) $(patsubst src/%,test/%,$(SOURCES))
+OBJECTS := $(SOURCES:.o=.c)
+TEST_OBJECTS = $(TEST_SOURCES:.o=.c)
+
+CC = avr-gcc
+CFLAGS = -g -Wall -Os -DF_CPU=$(CLOCK) -mmcu=$(DEVICE)
+
+TESTCC = gcc
+TESTCFLAGS = 
 
 AVRDUDE = avrdude -c $(PROGRAMMER) -P $(PROGRAMMER_PORT) -p $(PROGRAMMED_DEVICE)
-COMPILE = avr-gcc -g -Wall -Os -DF_CPU=$(CLOCK) -mmcu=$(DEVICE)
 
-all: $(BUILD_DIRECTORY)main.hex
+dump:
+	@echo $(SOURCES)
+	@echo
+	@echo $(TEST_SOURCES)
+	@echo
+	@echo $(OBJECTS)
+	@echo
+	@echo $(TEST_OBJECTS)
+
+all: $(BUILD_DIRECTORY)$(EXEC).hex
 
 clean:
-	rm -rf $(BUILD_DIRECTORY)* $(OBJECTS)
+	rm -rf $(BUILD_DIRECTORY)* $(OBJECTS) $(TEST_OBJECTS)
     
-disasm: $(BUILD_DIRECTORY)main.lss
+disasm: $(BUILD_DIRECTORY)$(EXEC).lss
 
 %.lss: %.elf
 	avr-objdump -d -S $< > $@
 
 $(SOURCE_DIRECTORY)%.o: $(SOURCE_DIRECTORY)%.c 
-	$(COMPILE) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
-$(BUILD_DIRECTORY)main.elf: $(OBJECTS)
-	$(COMPILE) -o $(BUILD_DIRECTORY)main.elf $(OBJECTS)
+$(BUILD_DIRECTORY)$(EXEC).elf: $(OBJECTS)
+	$(CC) $(CFLAGS) -o $(BUILD_DIRECTORY)$(EXEC).elf $(OBJECTS)
 
-$(BUILD_DIRECTORY)main.hex: $(BUILD_DIRECTORY)main.elf
-	avr-objcopy -j .text -j .data -O ihex $(BUILD_DIRECTORY)main.elf $(BUILD_DIRECTORY)main.hex
-	avr-size --format=avr --mcu=$(DEVICE) $(BUILD_DIRECTORY)main.elf
+$(BUILD_DIRECTORY)$(EXEC).hex: $(BUILD_DIRECTORY)$(EXEC).elf
+	avr-objcopy -j .text -j .data -O ihex $(BUILD_DIRECTORY)$(EXEC).elf $(BUILD_DIRECTORY)$(EXEC).hex
+	avr-size --format=avr --mcu=$(DEVICE) $(BUILD_DIRECTORY)$(EXEC).elf
 
 connect:
 	$(AVRDUDE)
 
-flash: $(BUILD_DIRECTORY)main.hex
-	$(AVRDUDE) -U flash:w:$(BUILD_DIRECTORY)main.hex:i
+flash: $(BUILD_DIRECTORY)$(EXEC).hex
+	$(AVRDUDE) -U flash:w:$(BUILD_DIRECTORY)$(EXEC).hex:i
 
 fuse:
 	$(AVRDUDE) $(FUSES)
+
+test: $(TEST_OBJECTS)
+	
+$(TEST_DIRECTORY)%.o: $(TEST_DIRECTORY)%.c
+	$(TESTCC) $(TESTCFLAGS) -c $< -o $@
---
+++ src/os-main.h
@@ -0,0 +1,134 @@
+/**
+ * OS
+ *
+ * Priority-based soft real-time multitasking operating system
+ *
+ * @author Jeff Stubler
+ * @date March 10, 2012
+ */
+
+#ifndef OS
+#define OS
+
+#include <inttypes.h>
+#include <string.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+//#include "atmega32.h"
+
+// TODO: Add preprocess check on various params
+/**
+ * Maximum number of processes that can be managed
+ */
+#define NUMBER_OF_PROCESSES 6
+
+/**
+ * Length of each time quantum (ms)
+ */
+#define QUANTUM_MILLISECOND_LENGTH 10
+
+#define NAME_SIZE 5
+
+/**
+ * Stack size
+ */
+#define IDLE_TASK_STACK_SIZE 64
+
+#define STACK_BLOCK_SIZE 32
+#define STACK_BLOCKS 16
+#define INIT_STACK_BLOCKS 4
+
+/**
+ * Semaphore structure
+ */
+
+typedef struct {
+    uint8_t count;
+    uint8_t wait_list[NUMBER_OF_PROCESSES];
+} os_semaphore;
+
+typedef struct {
+    uint8_t next, size;
+} os_stack_free_list_node;
+
+#define OS_FREE_LIST_NULL 255
+
+/* Critical sections */
+
+#define ENTER_CRITICAL_SECTION() \
+	uint8_t flags = SREG; \
+	asm volatile ("cli");
+
+#define ENTER_CRITICAL_SECTION_AGAIN() \
+    flags = SREG; \
+    asm volatile ("cli");
+	
+#define LEAVE_CRITICAL_SECTION() \
+	SREG = flags;
+
+/* Init task */
+
+#define INIT() \
+    void init_task(void) __attribute__ ((noreturn)); \
+    void init_task(void)
+
+/**
+ * Initialize operating system
+ */
+void os_init(void);
+
+/**
+ * Start multitasking with the timer ticker
+ */
+void os_start_ticker(void);
+
+/**
+ * Add new task to operating system
+ */
+int8_t os_add_task(void (*task)(void), volatile uint8_t *stack, uint8_t priority, char *name);
+
+/**
+ * Remove a task from the operating system
+ */
+int8_t os_remove_task(uint8_t pid);
+
+/**
+ * Get current task
+ */
+uint8_t os_get_current_pid(void);
+
+void copy_string(char *destination, uint8_t destination_size, char *source);
+
+/**
+ * Delay task for specified number of ticks
+ *
+ * @param pid Process ID to delay
+ * @param ticks Number of ticks to delay
+ */
+int8_t os_delay(uint8_t pid, uint32_t ticks);
+
+/**
+ * Cancel any delay on a task
+ * @param pid Process ID to cancel delay for
+ * @return Error code
+ */
+int8_t os_cancel_delay(uint8_t pid);
+
+void os_set_task_name(uint8_t pid, char *name);
+char *os_get_task_name(uint8_t pid);
+
+int8_t os_set_task_priority(uint8_t pid, uint8_t priority);
+int8_t os_get_task_priority(uint8_t pid);
+
+int8_t os_suspend_task(uint8_t pid);
+int8_t os_resume_task(uint8_t pid);
+
+void os_semaphore_init(os_semaphore *semaphore, uint8_t count);
+int8_t os_semaphore_wait(os_semaphore *semaphore);
+int8_t os_semaphore_signal(os_semaphore *semaphore);
+
+void os_lock_scheduler(void);
+void os_unlock_scheduler(void);
+
+#endif
